bfae0add70a2ec340457cb9bedb95658c51342fa
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2020-09-17 23:05:48 +0200

    Corrected format in deploy, started producing slides

--- R/deploy_material.R
+++ R/deploy_material.R
@@ -13,19 +13,19 @@
 #' \dontrun{
 #' ps_src <- file.path('vignettes', '00_preface.Rmd')
 #' ps_trg_dir <- file.path('docs', 'articles')
-#' ps_out_fmt <- 'all'
+#' ps_output_fmt <- 'all'
 #' deploy_material(ps_src, ps_trg_dir)
 #' }
 #'
 deploy_material <- function(ps_src, ps_trg_dir, ps_output_fmt = 'all'){
   # render source
-  rmarkdown::render(ps_src, ps_out_fmt)
+  rmarkdown::render(ps_src, ps_output_fmt)
   # check whether target exists
   if (!dir.exists(paths = ps_trg_dir)) dir.create(path = ps_trg_dir)
   # get source without ext
   s_src_noext <- fs::path_ext_remove(ps_src)
   # move output
-  if (ps_out_fmt == 'all'){
+  if (ps_output_fmt == 'all'){
     # assume html and pdf as formats
     for (f in c('pdf', 'html')){
       s_out_wext <- paste(s_src_noext, f, sep = '.')
@@ -33,7 +33,7 @@ deploy_material <- function(ps_src, ps_trg_dir, ps_output_fmt = 'all'){
       fs::file_move(path = s_out_wext, new_path = ps_trg_dir)
     }
   } else {
-    s_out_wext <- paste(s_src_noext, ps_out_fmt, sep = '.')
+    s_out_wext <- paste(s_src_noext, ps_output_fmt, sep = '.')
     if (!file.exists(s_out_wext)) stop(" *** ERROR: Cannot find output file: ", s_out_wext)
     fs::file_move(path = s_out_wext, new_path = ps_trg_dir)
   }
