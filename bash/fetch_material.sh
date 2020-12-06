e1f8d2c93b598e119c71c9d46b3b41eaf296fb9f
charlotte-ngs <ngs.charlotte@gmail.com>
2020-12-06 23:27:38 +0100

    Changed dir to sol11

--- bash/fetch_material.sh
+++ bash/fetch_material.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SCRIPTDIR=/home/quagadmin/source/misctools/
-DIRTOFETCH=ex/w12_vcomp
-#DIRTOFETCH=sol/w11_mem
+#DIRTOFETCH=ex/w12_vcomp
+DIRTOFETCH=sol/w12_vcomp
 TESTSTUDENT=/home/quagadmin/lbgfs2020/test-student-usernames-lbgfs2020.txt
 STUDENTUSER=/home/quagadmin/lbgfs2020/student-usernames-lbgfs2020.txt
 #' fetch individual directory
