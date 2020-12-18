37571dcda5bed0ad5d0b8e3fd396ead580bd81d5
charlotte-ngs <ngs.charlotte@gmail.com>
2020-12-18 06:39:19 +0100

    Started script to prepare e-mail messages

---
+++ rmdexam/e-mail/prep_e-mail.sh
@@ -0,0 +1,25 @@
+
+
+# wd
+cd Data/Projects/Github/charlotte-ngs/lbgfs2020_ghroot/master/lbgfs2020
+
+EMLDIR=rmdexam/e-mail
+EMLTMPL=rmdexam/e-mail/e-mail_template.txt
+
+# loop over students
+cat misc/20200918_exercise_platform/test_students_lbgfs2020.txt | while read e
+do
+  user=`echo $e | cut -d ',' -f1`
+  email=`echo $e | cut -d ',' -f2`
+  name=`echo $e | cut -d ',' -f3`
+  fname=`echo $e | cut -d ',' -f4`
+  echo " * User: $user"
+  echo " * E-mail: $email"
+  echo " * Name: $name"
+  echo " * Fname: $fname"
+  emailpath=$EMLDIR/"email_${user}.txt"
+  echo $emailpath
+  echo "Dear $fname $name" > $emailpath
+  cat $EMLTMPL >> $emailpath
+  sleep 2
+done
